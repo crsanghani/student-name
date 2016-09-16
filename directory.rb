dc136d28c7294e39ca92f5ed4066e946c16e44f7
Chetan Sanghani <crsanghani@gmail.com>
2016-09-16 17:08:07 +0100

    used center to style the output a little

--- directory.rb
+++ directory.rb
@@ -35,37 +35,44 @@ end
 
 #print the array of students
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(20)
+  border
+end
+
+#a centered line break to help with clarity
+def border
+  puts "-------------".center(20)
 end
 
 #iterating through the array to list student names
 def print(students)
   counter = 0
   until counter == students.length
-    puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)  Hobby: #{students[counter][:hobbies]}"
+    puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)  Hobby: #{students[counter][:hobbies]}".center(20)
     counter += 1
   end
+  border
 end
 
 
 #print the total number of students
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(20)
+  border
 end
 
 #shows the students that begin with a certain letter only
 def sort_students_by_letter(students)
   letter = "V"
   students_select = students.select{|student| student[:name][0] == letter}
-  puts students_select
+  puts students_select.center(20)
 end
 
 #shows the students that have less than 12 characters in their name
 def sort_students_by_length(students)
   length = 12
   students_select = students.select{|student| student[:name].length < 12}
-  puts students_select
+  puts students_select.center(20)
 end
 
 #calling the methods
