9bf1de1a9339d21fbf6f2c520be6b5757eec2243
Chetan Sanghani <crsanghani@gmail.com>
2016-09-16 14:19:02 +0100

    modified input_students to print a number with student names

--- directory.rb
+++ directory.rb
@@ -32,8 +32,8 @@ end
 
 #iterating through the array to list student names
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
